f24eadad3dbce83af1e706786cddcd4a9e0e356d
Anatoly Ivasenko <a.ivasenko@gmail.com>
2020-10-10 21:16:09 +0300

    Задание Multithreading. Драфт ThreadPool

--- TaskMultitreading/src/main/java/org/jschool/multithreading/ThreadPoolImpl.java
+++ TaskMultitreading/src/main/java/org/jschool/multithreading/ThreadPoolImpl.java
@@ -4,8 +4,9 @@ import java.util.*;
 
 public class ThreadPoolImpl implements ThreadPool{
     private final int numberOfThreads;
-    private List<Runnable> taskList = Collections.synchronizedList(new ArrayList<>());
-    private Map<Thread, Thread.State> threadMap = Collections.synchronizedMap(new HashMap<>());
+    private final Object taskListLock = new Object();
+    private final List<Runnable> taskList = Collections.synchronizedList(new ArrayList<>());
+    private final Map<Thread, Thread.State> threadMap = Collections.synchronizedMap(new HashMap<>());
 
 
     public ThreadPoolImpl() {
@@ -18,9 +19,29 @@ public class ThreadPoolImpl implements ThreadPool{
 
     @Override
     public void start() {
-        ThreadGroup pool = new ThreadGroup("pool");
+//        ThreadGroup pool = new ThreadGroup("pool");
         for (int i = 0; i < numberOfThreads; i++) {
-            Thread thread = new PoolsThread(pool,("pool-" + i));
+            Thread thread = new Thread(() -> {
+                Thread currentThread = Thread.currentThread();
+                String id = currentThread.getName();
+                System.out.println(id + " started");
+                while (!currentThread.isInterrupted()) {
+                    synchronized (this) {
+                        try {
+                            System.out.println(id + " going to wait");
+                            wait();
+                            System.out.println(id + " waked up");
+                            Runnable tempTask = taskList.remove(0);
+                            new Thread(tempTask);
+                            System.out.println(id + " task complete");
+                        } catch (InterruptedException e) {
+                            System.out.println(id + " interrupted");
+
+                        }
+                    }
+                }
+                System.out.println(id + " finished");
+            });
             thread.start();
             threadMap.put(thread, thread.getState());
         }
@@ -29,48 +50,79 @@ public class ThreadPoolImpl implements ThreadPool{
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.print(pool.getName() + ": " + pool.activeCount() + "---");
+//        System.out.print(pool.getName() + ": " + pool.activeCount() + "---");
         System.out.println(Thread.currentThread().getThreadGroup().getName() + ": " + Thread.currentThread().getThreadGroup().activeCount());
 
         threadMap.forEach((thread, state) ->
                 System.out.println(thread.getThreadGroup().getName() + "/"
                         + thread.getName() + ": " + thread.getState() + " " + state));
+//        synchronized (this) {
+//            threadMap.forEach((thread, state) -> notify());
+//        }
 
-        System.out.println(pool.getParent().getName());
 
 
+//        while (true) {
+//            try {
+//                synchronized (this) {
+//                    wait(500);
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            synchronized (this) {
+//                threadMap.forEach((thread, state) -> {
+//                    if (taskList.size() > 0) {
+//                        notify();
+//                    }
+//                });
+//            }
+//        }
     }
+
+
+
+
+
+
 
     @Override
     public void execute(Runnable task) {
-
-    }
-
-    private class PoolsThread extends Thread {
-
-        PoolsThread(ThreadGroup group, String name) {
-            super(group, name);
-        }
-
-        @Override
-        public void run(){
-            String id = getThreadGroup().getName() + "/" + getName();
-            System.out.println(id + " started");
-            while (!isInterrupted()){
-                synchronized (this) {
-                    try {
-
-                        System.out.println(id + " going to wait");
-                        wait();
-                    } catch (InterruptedException e) {
-                        System.out.println(id + " interrupted");
-                        interrupt();
-                    }
-                }
-            }
-            System.out.println(id + " finished");
+        synchronized (this) {
+            taskList.add(task);
         }
     }
-
-
 }
+
+//    private class PoolsThread extends Thread {
+//        private final Object locker;
+//        private List<Runnable> taskList;
+
+//        PoolsThread(ThreadGroup group, String name, List<Runnable> taskList, Object locker) {
+//            super(group, name);
+//            this.taskList = taskList;
+//            this.locker = locker;
+//        }
+//
+//        @Override
+//        public void run(){
+//            String id = getThreadGroup().getName() + "/" + getName();
+//            System.out.println(id + " started");
+//            while (!isInterrupted()){
+//                synchronized (locker) {
+//                    try {
+//                        System.out.println(id + " going to wait");
+//                        locker.wait();
+//                    } catch (InterruptedException e) {
+//                        System.out.println(id + " interrupted");
+//                        Runnable tempTask = taskList.remove(0);
+//                        new Thread(tempTask);
+//                    }
+//                }
+//            }
+//            System.out.println(id + " finished");
+//        }
+//    }
+//
+//
+//}
--- TaskMultitreading/src/test/java/RunTest.java
+++ TaskMultitreading/src/test/java/RunTest.java
@@ -1,10 +1,23 @@
 import org.jschool.multithreading.ThreadPool;
 import org.jschool.multithreading.ThreadPoolImpl;
 
+import java.util.List;
+
 public class RunTest {
 
     public static void main(String[] args) {
         ThreadPool threadPool = new ThreadPoolImpl();
         threadPool.start();
+
+
+        for (int i = 0; i < 200; i++) {
+            System.out.print("|");
+            int finalI = i;
+            threadPool.execute(() -> {
+                List<Long> triboResult = Tribonacci.tribonacci(finalI);
+                System.out.println(triboResult.get(triboResult.size()));
+            });
+        }
     }
 }
+
